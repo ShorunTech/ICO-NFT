e7a256fa2af2777da88564db15926ca7ce69ae95
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-08 15:24:12 +0100

    CryptoDevToken

--- hardhat-tutorial/contracts/CryptoDevToken.sol
+++ hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -9,4 +9,11 @@
       // Price of one Crypto Dev token
       uint256 public constant tokenPrice = 0.001 ether;
 
+       // Each NFT would give the user 10 tokens
+      // It needs to be represented as 10 * (10 ** 18) as ERC20 tokens are represented by the smallest denomination possible for the token
+      // By default, ERC20 tokens have the smallest denomination of 10^(-18). This means, having a balance of (1)
+      // is actually equal to (10 ^ -18) tokens.
+      // Owning 1 full token is equivalent to owning (10^18) tokens when you account for the decimal places.
+      // More information on this can be found in the Freshman Track Cryptocurrency tutorial.
+
       
