be8e1d2620b4020944220b7682f3199b557eef9b
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-09 09:28:42 +0100

    CryptoDevToken

--- hardhat-tutorial/contracts/CryptoDevToken.sol
+++ hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -8,14 +8,12 @@
   contract CryptoDevToken is ERC20, Ownable {
       // Price of one Crypto Dev token
       uint256 public constant tokenPrice = 0.001 ether;
-
-       // Each NFT would give the user 10 tokens
+      // Each NFT would give the user 10 tokens
       // It needs to be represented as 10 * (10 ** 18) as ERC20 tokens are represented by the smallest denomination possible for the token
       // By default, ERC20 tokens have the smallest denomination of 10^(-18). This means, having a balance of (1)
       // is actually equal to (10 ^ -18) tokens.
       // Owning 1 full token is equivalent to owning (10^18) tokens when you account for the decimal places.
       // More information on this can be found in the Freshman Track Cryptocurrency tutorial.
-
       uint256 public constant tokensPerNFT = 10 * 10**18;
       // the max total supply is 10000 for Crypto Dev Tokens
       uint256 public constant maxTotalSupply = 10000 * 10**18;
@@ -24,17 +22,16 @@
       // Mapping to keep track of which tokenIds have been claimed
       mapping(uint256 => bool) public tokenIdsClaimed;
 
-       constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+      constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
           CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
-
-    }
+      }
 
       /**
        * @dev Mints `amount` number of CryptoDevTokens
        * Requirements:
        * - `msg.value` should be equal or greater than the tokenPrice * amount
        */
-        function mint(uint256 amount) public payable {
+      function mint(uint256 amount) public payable {
           // the value of ether that should be equal or greater than tokenPrice * amount;
           uint256 _requiredAmount = tokenPrice * amount;
           require(msg.value >= _requiredAmount, "Ether sent is incorrect");
@@ -48,21 +45,20 @@
           _mint(msg.sender, amountWithDecimals);
       }
 
-          /**
-          * @dev Mints tokens based on the number of NFT's held by the sender
-          * Requirements:
-          * balance of Crypto Dev NFT's owned by the sender should be greater than 0
-          * Tokens should have not been claimed for all the NFTs owned by the sender
-          */
-
-         function claim() public {
+      /**
+       * @dev Mints tokens based on the number of NFT's held by the sender
+       * Requirements:
+       * balance of Crypto Dev NFT's owned by the sender should be greater than 0
+       * Tokens should have not been claimed for all the NFTs owned by the sender
+       */
+      function claim() public {
           address sender = msg.sender;
           // Get the number of CryptoDev NFT's held by a given sender address
           uint256 balance = CryptoDevsNFT.balanceOf(sender);
           // If the balance is zero, revert the transaction
           require(balance > 0, "You dont own any Crypto Dev NFT's");
           // amount keeps track of number of unclaimed tokenIds
-            uint256 amount = 0;
+          uint256 amount = 0;
           // loop over the balance and get the token ID owned by `sender` at a given `index` of its token list.
           for (uint256 i = 0; i < balance; i++) {
               uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
@@ -72,8 +68,6 @@
                   tokenIdsClaimed[tokenId] = true;
               }
           }
-
-
           // If all the token Ids have been claimed, revert the transaction;
           require(amount > 0, "You have already claimed all the tokens");
           // call the internal function from Openzeppelin's ERC20 contract
@@ -81,13 +75,12 @@
           _mint(msg.sender, amount * tokensPerNFT);
       }
 
-        /**
+      /**
         * @dev withdraws all ETH sent to this contract
         * Requirements:
         * wallet connected must be owner's address
         */
-
-         function withdraw() public onlyOwner {
+      function withdraw() public onlyOwner {
         uint256 amount = address(this).balance;
         require(amount > 0, "Nothing to withdraw; contract balance empty");
         
@@ -95,8 +88,11 @@
         (bool sent, ) = _owner.call{value: amount}("");
         require(sent, "Failed to send Ether");
       }
+
       // Function to receive Ether. msg.data must be empty
       receive() external payable {}
+
       // Fallback function is called when msg.data is not empty
       fallback() external payable {}
+
 }
